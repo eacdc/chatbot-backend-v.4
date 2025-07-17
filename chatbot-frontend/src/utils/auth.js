48fa87aa0f42777d23f1cfeeab14d7659b0dc383
eacdc <ea@cdcprinters.com>
2025-07-17 20:52:09 +0530

    Fix API_URL import errors for production build

--- chatbot-frontend/src/utils/auth.js
+++ chatbot-frontend/src/utils/auth.js
@@ -1,5 +1,5 @@
 import axiosInstance from './axios';
-import API_URL, { API_ENDPOINTS } from '../config';
+import { API_URL, API_ENDPOINTS } from '../config';
 
 // 30 minutes in milliseconds
 const SESSION_TIMEOUT = 30 * 60 * 1000;
